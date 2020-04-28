83ff11127ecc66bdaccae825b43e7b821f60d519
Muhummad Patel <muhummad.patel@gmail.com>
2020-04-28 21:28:36 +0200

    Remove motd component and functionality
    We're ready to start implementing the actual vLoader video downloading
    functionality so it's time to delete the last of the
    test/experimentation code: This commit gets rid of all motd-related code
    and configuration/state.

--- app/src/components/app/App.jsx
+++ app/src/components/app/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { writeConfigRequest } from "secure-electron-store";
-// import Motd from "Components/motd/Motd";
 import AddVideo from "Components/addVideo/AddVideo";
 import VideoList from "Components/videoList/VideoList";
 import "./app.scss";
@@ -8,22 +6,6 @@ import "./app.scss";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    const initialMotd = window.api.store.initial().motd;
-    this.state = {
-      motd: typeof initialMotd !== "undefined" ? initialMotd : "Default value",
-    };
-
-    this.updateMotd = this.updateMotd.bind(this);
-  }
-
-  updateMotd(motd) {
-    window.api.store.send(writeConfigRequest, "motd", motd);
-
-    // update our state
-    this.setState((state) => {
-      return { ...state, ...{ motd } };
-    });
   }
 
   render() {
